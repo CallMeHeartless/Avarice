dcad55dd514b5cdc9840251a1a76f381dd2026d0
Kerry <k.of.blades@gmail.com>
2018-09-10 14:27:05 +1200

    Kerry's initial stuff

---
+++ Avarice/Assets/Scripts/CoinDistractionPickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinDistractionPickup : MonoBehaviour {
+
+	
+    public void OnTriggerEnter(Collider collider) {
+        if (collider.CompareTag("Enemy")) {
+            Destroy(gameObject);
+        }
+    }
+
+}
