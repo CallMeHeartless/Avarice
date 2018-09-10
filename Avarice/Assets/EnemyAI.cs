34776f8569b74650ffcdbc226d63e6a629eb6d7e
MarximusPrime <39780696+MarximusPrime@users.noreply.github.com>
2018-09-10 16:47:22 +1200

    Raycast

---
+++ Avarice/Assets/EnemyAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+
+    public GameObject player;
+    public NavMeshAgent agent;
+
+    private Ray ray;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ray.origin = player.transform.position;
+        ray.direction = Vector3.down;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            agent.SetDestination(hit.point);
+        }
+		
+	}
+}
