ee17374e9425d23523524b42efdc130f564010b0
MarximusPrime <39780696+MarximusPrime@users.noreply.github.com>
2018-09-11 12:19:05 +1200

    Skeleton coin function

--- Avarice/Assets/EnemyAI.cs
+++ Avarice/Assets/EnemyAI.cs
@@ -9,15 +9,36 @@ public class EnemyAI : MonoBehaviour {
     public NavMeshAgent agent;
 
     private Ray ray;
+    private GameObject coin;
 
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    public GameObject FindClosestCoin()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Distraction");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
+    // Update is called once per frame
+    void Update () {
 
         ray.origin = player.transform.position;
         ray.direction = Vector3.down;
@@ -28,6 +49,22 @@ public class EnemyAI : MonoBehaviour {
         {
             agent.SetDestination(hit.point);
         }
-		
-	}
+
+        coin = FindClosestCoin();
+
+        if(coin != null)
+        {
+            ray.origin = coin.transform.position;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+
+            coin = null;
+        }
+
+        
+
+    }
 }
